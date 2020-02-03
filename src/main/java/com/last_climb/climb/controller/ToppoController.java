d0ff5440b6121eae68ad1d4bd01df51774ac3a0e
Jonathan Loiseau <jonatanloiseau@gmail.com>
2020-02-03 11:56:29 +0100

    First controllers + html

---
+++ src/main/java/com/last_climb/climb/controller/ToppoController.java
@@ -0,0 +1,14 @@
+package com.last_climb.climb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToppoController {
+
+	@GetMapping("/topo")
+	public String displayToppo(Model model) {
+		return "topo";
+	}
+}
